f743b91006a4ff68dbf25c4f99a07ac437bc645a
joach <jachym.pesek@gmail.com>
2017-05-14 11:20:05 +0200

    cmake structure test

---
+++ src/Configuration/Config.cpp
@@ -0,0 +1,31 @@
+/*
+#include <QtWidgets/QApplication>
+#include "Canvan.h"
+//#include "QntConfig.h"
+
+//using namespace Qnt;
+//using namespace ScIDE;
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	//app.setWindowIcon(QIcon("Qnt_AppIcon_16px.ico"));
+	//app.setStyle(QStyleFactory::create("Fusion"));
+	//app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+
+	Canvan win(100, 100, 800, 400);
+	win.setTitle("JuiPreview");
+	//win.setVersion(Qnt_VERSION_MAJOR, Qnt_VERSION_MINOR, Qnt_VERSION_PATCH);
+	win.show();
+
+
+	//app.setPalette(darkPalette);
+	win.msgConsole(QString("pre-bridge"));
+
+	//ScBridge bridge;
+	win.msgConsole(QString("bridge"));
+
+	
+	return app.exec();
+}
+*/
