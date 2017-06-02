e412d67437af5d46f9326d9e455e2073ac8dec71
J0ACH <jachym.pesek@gmail.com>
2017-06-02 09:30:03 +0200

    config build

--- src/Configuration/Config.cpp
+++ src/Configuration/Config.cpp
@@ -1,6 +1,7 @@
-/*
-#include <QtWidgets/QApplication>
-#include "Canvan.h"
+
+#include <QApplication>
+#include "Student.h"
+//#include "Canvan.h"
 //#include "QntConfig.h"
 
 //using namespace Qnt;
@@ -13,19 +14,22 @@ int main(int argc, char *argv[])
 	//app.setStyle(QStyleFactory::create("Fusion"));
 	//app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
-	Canvan win(100, 100, 800, 400);
-	win.setTitle("JuiPreview");
+	//Canvan win(100, 100, 800, 400);
+	//win.setTitle("JuiPreview");
 	//win.setVersion(Qnt_VERSION_MAJOR, Qnt_VERSION_MINOR, Qnt_VERSION_PATCH);
-	win.show();
+	//win.show();
 
 
 	//app.setPalette(darkPalette);
-	win.msgConsole(QString("pre-bridge"));
+	//win.msgConsole(QString("pre-bridge"));
 
 	//ScBridge bridge;
-	win.msgConsole(QString("bridge"));
+	//win.msgConsole(QString("bridge"));
 
+	Student s("Joeasd");
+	s.display();
+	getchar();
 	
 	return app.exec();
 }
-*/
+
